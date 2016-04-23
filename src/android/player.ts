ddb38faf30fad3a3faa8c5d7167d97f4c8ed54fb
Osei Fortune <fortune.osei@yahoo.com>
2016-04-23 00:12:01 -0400

    Add Android Methods

---
+++ src/android/player.ts
@@ -0,0 +1,10 @@
+import {Observable, EventData} from 'data/observable';
+import {TNSSpotifyConstants, TNSSpotifyTrackMetadataI, Utils} from '../common';
+import {TNSSpotifyAuth} from './auth';
+export class TNSSpotifyPlayer {
+    initPlayer(emitEvents?: boolean) {
+
+    }
+
+   
+}
